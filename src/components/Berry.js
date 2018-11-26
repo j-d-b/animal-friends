93ecf697b8fb03887bbeb56995a5fd10c4b6f207
j-d-b <jacob@jdbrady.info>
2018-11-26 12:16:11 -0500

    simplify berry component

--- src/components/Berry.js
+++ src/components/Berry.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Berry = ({ berry, isSelected, selectBerry }) => {
-  return (
-    <span
-      onClick={() => berry.type !== 'EMPTY' && selectBerry()}
-      style={{
-        backgroundColor: isSelected ? 'orange' : '',
-        cursor: 'pointer'
-      }}
-    >
-      {berry.image}
-      {isSelected && berry.description}
-    </span>
-  );
-};
+const Berry = ({ berry, isSelected, selectBerry }) => (
+  <span
+    onClick={() => berry.type !== 'EMPTY' && selectBerry()}
+    style={{
+      backgroundColor: isSelected ? 'orange' : '',
+      cursor: 'pointer'
+    }}
+  >
+    {berry.image}
+    {isSelected && berry.description}
+  </span>
+);
 
 export default Berry;
